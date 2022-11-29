c5ae470b3a46bd02e3c53cc8eb0d9a94eff57036
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-11-28 21:49:30 -0500

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import logo from "../assets/goLogo.png"
 import { Navbar } from "flowbite-react";
+import {Link} from "react-router-dom"
+
 
 function Navagation() {
   return (
     <div className="">
       <Navbar fluid={true} rounded={false} className="bg-zinc-700">
-        <Navbar.Brand href="/" className="">
-          <img
+        <Navbar.Brand className="">
+          <Link to="/"><img
             src={logo} 
             className="mr-3 h-6 sm:h-9 "
             alt="gareth-flynn-logo"
             style={{width: '80px', height: '80px'}}
-          />
+          /></Link>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
-          <Navbar.Link href="/" className="hover:opacity-50 text-slate-50	">About Me</Navbar.Link>
-          <Navbar.Link href ="/Portfolio-page" className="hover:opacity-50 text-slate-50">Portfolio</Navbar.Link>
-          <Navbar.Link href="/Resume" className="hover:opacity-50 text-slate-50	">Resume</Navbar.Link>
-          <Navbar.Link  href="/Contact" className="hover:opacity-50 text-slate-50	">Contact</Navbar.Link>
+          <Navbar.Link  className="hover:opacity-50 text-slate-50	"><Link to="/">About Me</Link></Navbar.Link>
+          <Navbar.Link  className="hover:opacity-50 text-slate-50"><Link to="/Portfolio-page">Portfolio</Link></Navbar.Link>
+          <Navbar.Link  className="hover:opacity-50 text-slate-50	"><Link to="/Resume">Resume</Link></Navbar.Link>
+          <Navbar.Link  className="hover:opacity-50 text-slate-50	"><Link to="/Contact">Contact</Link></Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
     </div>
